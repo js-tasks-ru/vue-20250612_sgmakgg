2fdae3b74d0992c2d13ed07f99a6ef6f990ca568
Sergey Makarov <sgmakgg@gmail.com>
2025-07-02 16:13:57 +0300

    task MeetupCover with v-bind

--- 04-sfc/20-MeetupCover-style-v-bind/MeetupCover.vue
+++ 04-sfc/20-MeetupCover-style-v-bind/MeetupCover.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import {computed} from 'vue'
 
 const props = defineProps({
   title: {
@@ -11,11 +11,15 @@ const props = defineProps({
   },
 })
 
-const bgStyle = computed(() => (props.image ? { '--bg-url': `url('${props.image}')` } : undefined))
+const bgImage = computed(() =>
+    props.image
+        ? `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('${props.image}')`
+        : `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover)`
+);
 </script>
 
 <template>
-  <div class="meetup-cover" :style="bgStyle">
+  <div class="meetup-cover">
     <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
@@ -26,7 +30,7 @@ const bgStyle = computed(() => (props.image ? { '--bg-url': `url('${props.image}
   background-position: center;
   /* Если изображение присутствует - берём его из CSS переменной, установленной на элемент в шаблоне */
   /* Иначе выводим изображение по умолчанию - var(--default-cover) */
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--bg-url, var(--default-cover));
+  background-image: v-bind(bgImage);
   display: flex;
   flex-direction: column;
   align-items: center;
