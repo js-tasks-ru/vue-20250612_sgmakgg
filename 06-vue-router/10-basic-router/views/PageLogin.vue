fd661dc3976f2174972a77357b58c41ac086d7ff
Sergey Makarov <sgmakgg@gmail.com>
2025-07-10 17:37:40 +0300

    task basic-router

--- 06-vue-router/10-basic-router/views/PageLogin.vue
+++ 06-vue-router/10-basic-router/views/PageLogin.vue
@@ -1,17 +1,23 @@
-<script setup lang="ts">
-import { UiButton, UiFormGroup, UiInput } from '@shgk/vue-course-ui'
-import { ref } from 'vue'
+<script lang="ts" setup>
+import {UiButton, UiFormGroup, UiInput} from '@shgk/vue-course-ui'
+import {ref} from 'vue'
 import MeetupsAuthForm from '../components/MeetupsAuthForm.vue'
 import LayoutAuth from '../components/LayoutAuth.vue'
-import { login } from '../api.ts'
+import {login} from '../api.ts'
+import {useRoute, useRouter} from "vue-router";
 
 const email = ref('demo@email')
 const password = ref('password')
+
+const router = useRouter()
+const route = useRoute()
 
 async function onSubmit() {
   try {
     await login(email.value, password.value)
     // Авторизация прошла успешно
+    const redirectTo = route.query.from?.toString() || '/'
+    await router.push(redirectTo)
   } catch (error) {
     alert((error as Error).message)
   }
@@ -22,20 +28,20 @@ async function onSubmit() {
   <LayoutAuth title="Вход">
     <MeetupsAuthForm @submit="onSubmit">
       <UiFormGroup label="Email">
-        <UiInput v-model="email" name="email" type="email" placeholder="demo@email" large required />
+        <UiInput v-model="email" large name="email" placeholder="demo@email" required type="email" />
       </UiFormGroup>
 
       <UiFormGroup label="Пароль">
-        <UiInput v-model="password" name="password" type="password" placeholder="password" large required />
+        <UiInput v-model="password" large name="password" placeholder="password" required type="password" />
       </UiFormGroup>
 
       <template #submit>
-        <UiButton kind="primary" type="submit" wide size="large">Войти</UiButton>
+        <UiButton kind="primary" size="large" type="submit" wide>Войти</UiButton>
       </template>
 
       <template #append>
         Нет аккаунта?
-        <a href="/register">Зарегистрируйтесь</a>
+        <RouterLink to="/register">Зарегистрируйтесь</RouterLink>
       </template>
     </MeetupsAuthForm>
   </LayoutAuth>
--- 06-vue-router/10-basic-router/views/PageRegister.vue
+++ 06-vue-router/10-basic-router/views/PageRegister.vue
@@ -1,15 +1,18 @@
-<script setup lang="ts">
-import { UiButton, UiCheckbox, UiFormGroup, UiInput } from '@shgk/vue-course-ui'
-import { ref } from 'vue'
+<script lang="ts" setup>
+import {UiButton, UiCheckbox, UiFormGroup, UiInput} from '@shgk/vue-course-ui'
+import {ref} from 'vue'
 import MeetupsAuthForm from '../components/MeetupsAuthForm.vue'
 import LayoutAuth from '../components/LayoutAuth.vue'
-import { register } from '../api.ts'
+import {register} from '../api.ts'
+import {useRouter} from "vue-router";
 
 const email = ref('demo@email')
 const fullname = ref('Demo Organizer')
 const password = ref('password')
 const password2 = ref('password')
 const agree = ref(true)
+
+const router = useRouter()
 
 async function onSubmit() {
   try {
@@ -19,6 +22,7 @@ async function onSubmit() {
       password: password.value,
     })
     // Регистрация прошла успешно
+    await router.push('/login')
   } catch (error) {
     alert((error as Error).message)
   }
@@ -29,7 +33,7 @@ async function onSubmit() {
   <LayoutAuth title="Регистрация">
     <MeetupsAuthForm @submit="onSubmit">
       <UiFormGroup label="Email">
-        <UiInput v-model="email" name="email" type="email" placeholder="demo@email" required />
+        <UiInput v-model="email" name="email" placeholder="demo@email" required type="email" />
       </UiFormGroup>
 
       <UiFormGroup label="Имя">
@@ -37,22 +41,22 @@ async function onSubmit() {
       </UiFormGroup>
 
       <UiFormGroup label="Пароль">
-        <UiInput v-model="password" name="password" type="password" required minlength="6" placeholder="••••••" />
+        <UiInput v-model="password" minlength="6" name="password" placeholder="••••••" required type="password" />
       </UiFormGroup>
 
       <UiFormGroup label="Повтор пароля">
-        <UiInput v-model="password2" type="password" required minlength="6" placeholder="••••••" />
+        <UiInput v-model="password2" minlength="6" placeholder="••••••" required type="password" />
       </UiFormGroup>
 
       <UiCheckbox v-model="agree" name="agree" required>Я согласен с условиями</UiCheckbox>
 
       <template #submit>
-        <UiButton kind="primary" size="large" wide type="submit">Зарегистрироваться</UiButton>
+        <UiButton kind="primary" size="large" type="submit" wide>Зарегистрироваться</UiButton>
       </template>
 
       <template #append>
         Уже есть аккаунт?
-        <a href="/login">Войдите</a>
+        <RouterLink to="/login">Войдите</RouterLink>
       </template>
     </MeetupsAuthForm>
   </LayoutAuth>
